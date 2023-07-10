adbac362146a8abd74ad8f65f0243db78e5c58c4
franck beya <fbeya@atecna.fr>
2023-07-10 14:14:10 +0200

    fix variable vite

--- src/api.ts
+++ src/api.ts
@@ -1,19 +1,13 @@
 import axios from 'axios'
 
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-
 interface DefaultOptions {
   baseURL: string | undefined
   headers?: { 'Content-Type'?: string; Authorization?: string }
 }
 
-const API_URL = import.meta.env.VITE_URL
-
 export default function fetchClient() {
   const defaultOptions: DefaultOptions = {
-    baseURL: API_URL,
+    baseURL: import.meta.env.VITE_URL,
     headers: {
       'Content-Type': 'application/json',
     },
--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,6 @@ import react from '@vitejs/plugin-react'
 // import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  define: {
-    'windows.process': { cwd: () => '' },
-    'process.env': {
-      VITE_URL: 'https://virtserver.swaggerhub.com/amelique-atecna/Atecna-workout/1.0.0',
-    },
-  },
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
