bda3ad575692a399667724f650d1d99677701e3f
franck beya <fbeya@atecna.fr>
2023-07-31 23:36:47 +0200

    add rules form text input

--- src/components/Connexion/Connexion.tsx
+++ src/components/Connexion/Connexion.tsx
@@ -4,6 +4,7 @@ import sport from '../../public/images/sport.svg'
 import PasswordInput from '~/elements/PasswordInput/PasswordInput'
 import Button from '~/elements/Button/Button'
 import TextInput from '~/elements/TextInput/TextInput'
+import {rules} from "~/utils/formRules";
 
 interface formInputs {
   name: string
@@ -14,7 +15,7 @@ interface formInputs {
 
 const Connexion = () => {
   const [showRegister, setShowRegister] = useState<boolean>(false)
-  const { handleSubmit, control } = useForm<formInputs>({ mode: 'onSubmit' })
+  const { handleSubmit, register, formState: {errors, touchedFields}, control } = useForm<formInputs>({ mode: 'onSubmit' })
   const onSubmit = (data: formInputs) => {
     console.log(data)
   }
@@ -24,7 +25,7 @@ const Connexion = () => {
                <div className="flex flex-col items-center">
                    <img className="w-[7rem] mt-[4rem]" src={sport} alt=""/>
                    <p className="text-white font-bold text-[1.2rem] mt-4">No time to rest, Booya !</p>
-               </div>
+              </div>
                <div className="px-6 -mt-8">
                    <h1 className="text-white font-bold">{showRegister ? <span className="text-[1.8rem]">Création de compte</span> : <span className="text-[2.8rem]">Bienvenue !</span>}</h1>
                    <p className="text-white text-[1rem] mt-2">{showRegister ? 'Veuillez remplir les champs suivant : ' : 'Vous pouvez vous connecter : '}</p>
@@ -33,14 +34,14 @@ const Connexion = () => {
             <div className="bg-colorHome pb-4 h-full pt-6">
                 {showRegister
                   ? <form className=" px-6" onSubmit={handleSubmit(onSubmit)}>
-                        <TextInput name="name" label="Nom du compte" type="text" data="input-name-register" placeholder="Nom du compte" />
-                        <TextInput name="Email" type="email" label="email" className="mb-5" />
+                        <TextInput {...register('name', rules.name)} isValid={touchedFields.name && !errors.name} isNotValid={errors.name} messageError={errors.name && errors.name.message} name="name" label="Nom du compte" type="text" data="input-name-register" placeholder="Nom du compte" />
+                        <TextInput {...register('email', rules.email)} isValid={touchedFields.email && !errors.email} isNotValid={errors.email} messageError={errors.email && errors.email.message} name="Email" type="email" label="email" className="mb-5" />
                         <PasswordInput control={control} label="Mot de passe" name="password" />
                         <PasswordInput control={control} label="Retapez votre mot de passe" name="confirm" />
                         <Button text='Creer mon compte' className="text-white" />
                     </form>
                   : <form className=" px-6 relative">
-                        <TextInput name="name" type="text" data="input-name-login" label="Nom du compte" className="mb-5" placeholder="Nom du compte" />
+                        <TextInput {...register('name', rules.name)} isValid={touchedFields.name && !errors.name} isNotValid={errors.name} messageError={errors.name && errors.name.message} name="name" type="text" data="input-name-login" label="Nom du compte" className="mb-5" placeholder="Nom du compte" />
                         <PasswordInput control={control} label="Mot de passe" name="password" />
                         <a className='no-underline text-primary-1 font-bold -mt-6 mb-9 block text-[0.9rem]'>Mot de passe oublié</a>
                         <Button text='Me connecter' className="text-white" />
--- src/elements/TextInput/TextInput.tsx
+++ src/elements/TextInput/TextInput.tsx
@@ -7,19 +7,28 @@ export interface Props {
   type: string
   defaultValue?: string
   id?: string
-  error?: boolean
   helperText?: string
   className?: string
   control?: any
   placeholder?: any
   data?: string
+  isValid?: boolean
+  isNotValid?: any
+  messageError?: string
 }
 
-const TextInput = ({ name, type, label, placeholder, data }: Props) => {
+const classNames = (...classes: any) => {
+  return classes.join(' ')
+}
+
+const TextInput = ({ name, type, label, placeholder, data, isValid, isNotValid, messageError }: Props) => {
   return (
         <div className={style.form__group}>
-          <input type={type} id={name} data-cy={data} className={style.form__field} placeholder={placeholder} />
+          <input type={type} id={name} data-cy={data}
+                  className={classNames(style.form__field, isValid && 'text-indigo-600', isNotValid && 'text-red-600')}
+                  placeholder={placeholder} />
             <label htmlFor={name} className={style.form__label}>{label}</label>
+          {isNotValid && <p className="text-red-600">{messageError}</p>}
         </div>
   )
 }
